676bb5c30f0e800fd47931d977b1c7d0b447585c
alpesh <sanghanialpesh@gmail.com>
2017-07-26 23:47:50 -0500

    comment removed-eclipse

--- charterApp/src/main/java/com/delta/charter/CharterAppApplication.java
+++ charterApp/src/main/java/com/delta/charter/CharterAppApplication.java
@@ -14,7 +14,7 @@ import com.delta.charter.service.UserService;
 
 @SpringBootApplication
 public class CharterAppApplication implements CommandLineRunner {
-	//test commit
+
 	@Autowired
 	private UserService userService;
 	
