f8fa1b279dca9146d27ebb7053422a1b19fab21f
alpesh <sanghanialpesh@gmail.com>
2017-07-30 20:49:55 -0500

    profile message added

--- charterApp/src/main/java/com/delta/charter/CharterAppApplication.java
+++ charterApp/src/main/java/com/delta/charter/CharterAppApplication.java
@@ -1,6 +1,7 @@
 package com.delta.charter;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,9 +25,12 @@ public class CharterAppApplication implements CommandLineRunner {
 	public static void main(String[] args) {
 		SpringApplication.run(CharterAppApplication.class, args);
 	}
-//test commit
+	@Value("${environmentname.file}")
+	private String env;
+
 	@Override
 	public void run(String... arg0) throws Exception {
+		System.out.println("Profile:---->" + env);
 		/*CharterUser chuser = new CharterUser();
 		chuser.setUsername("e");;
 		chuser.setFirstname("Alpesh");
