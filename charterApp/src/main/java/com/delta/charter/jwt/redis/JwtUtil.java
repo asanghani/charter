4403c3c66689042a2431d1e9e57026c7e9de6de1
046997 <046997@XMSPBLDJ00871.delta.rl.delta.com>
2017-11-08 13:25:48 -0600

    JWT Enhancement

---
+++ charterApp/src/main/java/com/delta/charter/jwt/redis/JwtUtil.java
@@ -0,0 +1,52 @@
+package com.delta.charter.jwt.redis;
+
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+public class JwtUtil {
+    private static final String REDIS_SET_ACTIVE_SUBJECTS = "active-subjects";
+
+    public static String generateToken(String signingKey, String subject) {
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        JwtBuilder builder = Jwts.builder()
+                .setSubject(subject)
+             //   .setIssuedAt(now)
+                .signWith(SignatureAlgorithm.HS256, signingKey);
+
+        String token = builder.compact();
+
+    //    RedisUtil.INSTANCE.sadd(REDIS_SET_ACTIVE_SUBJECTS, subject);
+        RedisUtil.INSTANCE.sadd(subject,token);
+
+        return token;
+    }
+
+    public static String parseToken(HttpServletRequest httpServletRequest, String jwtTokenCookieName, String signingKey){
+       // String token = CookieUtil.getValue(httpServletRequest, jwtTokenCookieName);
+    	String token = null;
+        if(token == null) {
+            return null;
+        }
+
+        String subject = Jwts.parser().setSigningKey(signingKey).parseClaimsJws(token).getBody().getSubject();
+   //     if (!RedisUtil.INSTANCE.sismember(REDIS_SET_ACTIVE_SUBJECTS, subject)) {
+        if (!RedisUtil.INSTANCE.sismember(subject,token)) {
+            return null;
+        }
+
+        return subject;
+    }
+
+    public static void invalidateRelatedTokens(HttpServletRequest httpServletRequest) {
+        RedisUtil.INSTANCE.srem(REDIS_SET_ACTIVE_SUBJECTS, (String) httpServletRequest.getAttribute("username"));
+    //	RedisUtil.INSTANCE.srem(subject,token);
+    	
+    }
+}
+
---
+++ charterApp/src/main/java/com/delta/charter/jwt/redis/RedisUtil.java
@@ -0,0 +1,52 @@
+package com.delta.charter.jwt.redis;
+
+import redis.clients.jedis.Jedis;
+import redis.clients.jedis.JedisPool;
+import redis.clients.jedis.JedisPoolConfig;
+
+public enum RedisUtil {
+    INSTANCE;
+
+    private final JedisPool pool;
+
+    RedisUtil() {
+        pool = new JedisPool(new JedisPoolConfig(), "localhost");
+    }
+
+    public void sadd(String key, String value) {
+        Jedis jedis = null;
+        try{
+            jedis = pool.getResource();
+            jedis.sadd(key, value);
+            jedis.expire(key, 60);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public void srem(String key, String value) {
+        Jedis jedis = null;
+        try{
+            jedis = pool.getResource();
+            jedis.srem(key, value);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+
+    public boolean sismember(String key, String value) {
+        Jedis jedis = null;
+        try{
+            jedis = pool.getResource();
+            return jedis.sismember(key, value);
+        } finally {
+            if (jedis != null) {
+                jedis.close();
+            }
+        }
+    }
+}
